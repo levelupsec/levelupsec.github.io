240b99edb5cef16b341227533065339a0681e3ea
samthode <sam.thode@gmail.com>
2024-01-22 12:49:01 -0500

    Initial Site Build

---
+++ assets/js/company_json.js
@@ -0,0 +1,35 @@
+const company_json = {
+    "logoPosition": "right",
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "text",
+        "name": "question1",
+        "title": "Name",
+        "isRequired": true
+       },
+       {
+        "type": "text",
+        "name": "question2",
+        "title": "Email",
+        "isRequired": true,
+        "inputType": "email"
+       },
+       {
+        "type": "text",
+        "name": "question3",
+        "title": "Company",
+        "isRequired": true
+       },
+       {
+        "type": "text",
+        "name": "question4",
+        "title": "Skills you're looking for",
+        "isRequired": true
+       }
+      ]
+     }
+    ]
+   }
